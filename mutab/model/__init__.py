4bb2767bea4700728c7163bc70f8ae1d901a3e90
JG1VPP <jg1vpp@gmail.com>
2025-12-30 23:48:36 +0900

    mmengine

--- mutab/model/__init__.py
+++ mutab/model/__init__.py
@@ -1,7 +1,6 @@
 from .backbone import TableResNet
 from .decoder import TableDecoder
 from .encoder import TableEncoder
-from .factory import build as build_detector
 from .handler import TableHandler
 from .network import Decoder, Fetcher, Locator
 from .revisor import TableRevisor
@@ -17,5 +16,4 @@ __all__ = [
     "TableResNet",
     "TableRevisor",
     "TableScanner",
-    "build_detector",
 ]
